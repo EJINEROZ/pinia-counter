8cfef7e967781e349a589768ff7a8a7b9aa02d93
Ejiro Uweghrepha <ejineroz@Ejiros-MacBook-Air.local>
2023-02-08 16:19:40 +0100

    first commit

---
+++ src/stores/counter.js
@@ -0,0 +1,33 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+
+// export const useCounterStore = defineStore('counter', () => {
+//   const count = ref(0)
+//   const doubleCount = computed(() => count.value * 2)
+//   function increment() {
+//     count.value++
+//   }
+
+//   return { count, doubleCount, increment }
+// })
+
+export const useCounterStore = defineStore({
+  id: 'counter',
+  state: () => ({
+    count: 0
+  }),
+  actions: {
+    increaseCount() {
+      this.count++
+    },
+    decreaseCount() {
+      this.count--
+    }
+  },
+  getters: {
+    oddOrEven: (state) => {
+      if (state.count % 2 === 0) return 'even'
+      return 'odd'
+    }
+  }
+})
